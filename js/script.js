ec879298810acd91f43418b57f074fae7adfa5f2
francaparroz21 <francaparroz21@gmail.com>
2022-11-23 22:25:25 -0300

    modificando titulo del index html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Algoritmo para saber si una palabra es palindromo (si se lee de izquierda a derecha y viceversa).
+//Algoritmo para saber si una palabra es palindromo, es decir,si se lee de izquierda a derecha y viceversa (for).
 
 function isPalindromic(word){
     for(let i = 0;i<word.length;i++){
@@ -11,8 +11,7 @@ function result(boolean){
     return "The word is not palindromic.";
 }
 
-
+//Creamos la variable user como nombre y word que es la palabra. Luegos hacemos un alert saludando y retornando el resultado de la palabra
 let user = prompt("Please enter your name.");
 let word = prompt("Enter the word. ");
 alert("Hi "+ user+"!\n" + result(isPalindromic(word)));
-
